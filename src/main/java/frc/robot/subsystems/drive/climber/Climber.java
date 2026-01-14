7ca738d5469ec34358de572c6cdbe2c3afb475c5
chris park <chrispy1729@gmail.com>
2026-01-14 18:29:19 -0500

    Implemented Climber.java folder

---
+++ src/main/java/frc/robot/subsystems/drive/climber/Climber.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.drive.climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.W8.mechanisms.linear.LinearMechanism;
+
+public class Climber extends SubsystemBase {
+  private LinearMechanism _io;
+
+  public Climber(LinearMechanism io) {
+    io = _io;
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
