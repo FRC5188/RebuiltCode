5e1b4e2e6da3dec354310b63fd382fe5114790a9
rhit-halseysh <119546114+rhit-halseysh@users.noreply.github.com>
2026-01-14 19:11:47 -0500

    Fix location

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.drive.climber;
+package frc.robot.subsystems.climber;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.W8.mechanisms.linear.LinearMechanism;
