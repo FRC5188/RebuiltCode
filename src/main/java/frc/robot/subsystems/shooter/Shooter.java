4b44e253ca369bc492284bb82fc09c113ba8fd9c
chris park <chrispy1729@gmail.com>
2026-01-17 15:37:35 -0500

    Added flywheel code and feeder code. Added constants to constants file as well

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,17 +1,58 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.W8.io.motor.MotorIO.PIDSlot;
 import frc.lib.W8.mechanisms.flywheel.FlywheelMechanism;
+import frc.robot.Constants.FeederConstants;
+import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
-    private FlywheelMechanism _io;
+  private FlywheelMechanism _flywheel;
+  private FlywheelMechanism _feeder;
+  public double desiredVelo;
 
-    public Shooter(FlywheelMechanism io) {
-        _io = io;
-    }
+  public Shooter(FlywheelMechanism flywheel, FlywheelMechanism feeder) {
+    _flywheel = flywheel;
+    _feeder = feeder;
+  }
 
-    @Override
-    public void periodic() {
+  // Sets feeder motor speed
+  public void runFeeder() {
+    _feeder.runVelocity(
+        FeederConstants.FEED_SPEED, FeederConstants.FEED_ACCELERATION, PIDSlot.SLOT_2);
+  }
 
-    }
+  // Sets the flywheel velocity based on an input.
+  public void setFlywheelVelocity(double velocity) {
+    AngularVelocity angVelo = RotationsPerSecond.of(velocity);
+    velocity = desiredVelo;
+
+    _flywheel.runVelocity(angVelo, ShooterConstants.ACCELERATION, PIDSlot.SLOT_0);
+  }
+
+  // Checks if the flywheel is at speed and returns a boolean
+  public boolean flyAtVelocity() {
+    return Math.abs(desiredVelo - _flywheel.getVelocity().in(RotationsPerSecond))
+        <= ShooterConstants.FLYWHEEL_VELOCITY_TOLERANCE;
+  }
+
+  public Command shoot(double velocity) {
+    return Commands.run(
+            () -> {
+              setFlywheelVelocity(velocity);
+            })
+        .andThen(
+            () -> {
+              runFeeder();
+            })
+        .unless(() -> (flyAtVelocity() == false));
+  }
+
+  @Override
+  public void periodic() {}
 }
