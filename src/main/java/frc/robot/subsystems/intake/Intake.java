ba7417484c897db77bc78a04f1844137fc8c27ff
chris park <chrispy1729@gmail.com>
2026-01-19 18:51:07 -0500

    Added intake states using an enum

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.intake;
 
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.W8.io.motor.MotorIO.PIDSlot;
 import frc.lib.W8.mechanisms.flywheel.FlywheelMechanism;
@@ -13,6 +15,18 @@ public class Intake extends SubsystemBase {
 
   public Intake(FlywheelMechanism io) {
     _io = io;
+  }
+  
+  public enum State {
+    NONE(Units.RadiansPerSecond.of(0.0)),
+    PULL(Units.RadiansPerSecond.of(1.0)),
+    EXPELL(Units.RadiansPerSecond.of(-1.0));
+
+    final AngularVelocity stateVelocity;
+
+    State(AngularVelocity stateVelocity) {
+      this.stateVelocity = stateVelocity;
+    }
   }
 
   public void setVelocity(double velocity) {
