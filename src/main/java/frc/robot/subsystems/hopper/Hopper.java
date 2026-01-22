30ae98492e1518dd70eccc5e34b7074910236e69
Ghost <145223182+GhostR406@users.noreply.github.com>
2026-01-21 20:21:11 -0500

    WIP started State Enum!

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -3,9 +3,28 @@ package frc.robot.subsystems.hopper;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.W8.mechanisms.flywheel.FlywheelMechanism;
 
-public class Hopper extends SubsystemBase {
+import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units;
 
+public class Hopper extends SubsystemBase {
     private FlywheelMechanism _io;
+
+    public enum State
+    {
+        OFF /*(Units.RevolutionsPerSecond.of(0.0))*/,
+        FORWARD_SLOW /*(Units.RevolutionsPerSecond.of(SLOW_SPEED_RPM/60))*/,
+        FORWARD_FAST /*(Units.RevolutionsPerSecond.of(FAST_SPEED_RPM/60))*/,
+        REVERSE /*(Units.RevolutionsPerSecond.of(REVERSE_SPEED_RPM/60))*/;
+
+        private final AngularVelocity _stateVelocity;
+
+        public State(AngularVelocity stateVelocity) {
+            _stateVelocity = stateVelocity;
+        }
+
+        // CREATE CONSTANTS FOR RPM OF EACH STATE IN HopperConstants !
+
+    }
 
     public Hopper(FlywheelMechanism io) {
         _io = io;
