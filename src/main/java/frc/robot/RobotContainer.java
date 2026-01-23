d783db146b5e06e38d8cbdeb4108c5c84dace947
Ghost <145223182+GhostR406@users.noreply.github.com>
2026-01-22 20:01:27 -0500

    WIP 1/22/26 Instantiate Hopper Subsystem and Command!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,14 +15,20 @@ package frc.robot;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.lib.W8.io.motor.*;
+import frc.lib.W8.mechanisms.flywheel.*;
+import frc.lib.W8.util.Device.CAN;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.drive.Drive;
@@ -31,132 +37,140 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.hopper.Hopper;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+* This class is where the bulk of the robot should be declared. Since Command-based is a
+* "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+* periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+* subsystems, commands, and button mappings) should be declared here.
+*/
 public class RobotContainer {
-  // Subsystems
-  private final Drive drive;
+// Subsystems
+private final Drive drive;
+//private final Hopper hopper;
 
-  // Controller
-  private final CommandXboxController controller = new CommandXboxController(0);
+// Controller
+private final CommandXboxController controller = new CommandXboxController(0);
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
+// Dashboard inputs
+private final LoggedDashboardChooser<Command> autoChooser;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
-        break;
+/** The container for the robot. Contains subsystems, OI devices, and commands. */
+public RobotContainer() {
+switch (Constants.currentMode) {
+case REAL:
+// Real robot, instantiate hardware IO implementations
+drive =
+new Drive(
+new GyroIOPigeon2(),
+new ModuleIOTalonFX(TunerConstants.FrontLeft),
+new ModuleIOTalonFX(TunerConstants.FrontRight),
+new ModuleIOTalonFX(TunerConstants.BackLeft),
+new ModuleIOTalonFX(TunerConstants.BackRight));
 
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
-        break;
+/*hopper =
+new Hopper(new FlywheelMechanismReal(new MotorIORev("HopperMotor", new CAN(0, "rio"), false, null, null)));*/
+break;
 
-      default:
-        // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        break;
-    }
+case SIM:
+// Sim robot, instantiate physics sim IO implementations
+drive =
+new Drive(
+new GyroIO() {},
+new ModuleIOSim(TunerConstants.FrontLeft),
+new ModuleIOSim(TunerConstants.FrontRight),
+new ModuleIOSim(TunerConstants.BackLeft),
+new ModuleIOSim(TunerConstants.BackRight));
 
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+/*hopper =
+new Hopper(new FlywheelMechanismSim(new MotorIOSim("HopperMotor", new CAN(0, "rio"), false, null, null)), new DCMotor(0, 0, 0, 0, 0, 0));*/
+break;
 
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+default:
+// Replayed robot, disable IO implementations
+drive =
+new Drive(
+new GyroIO() {},
+new ModuleIO() {},
+new ModuleIO() {},
+new ModuleIO() {},
+new ModuleIO() {});
+break;
+}
 
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+// Set up auto routines
+autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> controller.getLeftY(),
-            () -> controller.getLeftX(),
-            () -> -controller.getRightX()));
+// Set up SysId routines
+autoChooser.addOption(
+"Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+autoChooser.addOption(
+"Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+autoChooser.addOption(
+"Drive SysId (Quasistatic Forward)",
+drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+autoChooser.addOption(
+"Drive SysId (Quasistatic Reverse)",
+drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+autoChooser.addOption(
+"Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+autoChooser.addOption(
+"Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
-    // Lock to 0° when A button is held
-    controller
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY(),
-                () -> -controller.getLeftX(),
-                () -> new Rotation2d()));
+// Configure the button bindings
+configureButtonBindings();
+}
 
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+/**
+* Use this method to define your button->command mappings. Buttons can be created by
+* instantiating a {@link GenericHID} or one of its subclasses ({@link
+* edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+* edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+*/
+private void configureButtonBindings() {
+// Default command, normal field-relative drive
+drive.setDefaultCommand(
+DriveCommands.joystickDrive(
+drive,
+() -> controller.getLeftY(),
+() -> controller.getLeftX(),
+() -> -controller.getRightX()));
 
-    // Reset gyro to 0° when B button is pressed
-    controller
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
-  }
+// Lock to 0° when A button is held
+controller
+.a()
+.whileTrue(
+DriveCommands.joystickDriveAtAngle(
+drive,
+() -> -controller.getLeftY(),
+() -> -controller.getLeftX(),
+() -> new Rotation2d()));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autoChooser.get();
-  }
+// Switch to X pattern when X button is pressed
+controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-  TalonFX billyBob = new TalonFX(30, TunerConstants.DrivetrainConstants.CANBusName);
+// Reset gyro to 0° when B button is pressed
+controller
+.b()
+.onTrue(
+Commands.runOnce(
+() ->
+drive.setPose(
+new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+drive)
+.ignoringDisable(true));
+}
+
+/**
+* Use this to pass the autonomous command to the main {@link Robot} class.
+*
+* @return the command to run in autonomous
+*/
+public Command getAutonomousCommand() {
+return autoChooser.get();
+}
+
+TalonFX billyBob = new TalonFX(30, TunerConstants.DrivetrainConstants.CANBusName);
 }
