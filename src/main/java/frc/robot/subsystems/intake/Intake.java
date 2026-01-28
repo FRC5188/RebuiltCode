86bab346bd4e4ad3bffaa500f5bee967341f7d70
shlap <Chrispy1729@gmail.com>
2026-01-28 18:16:02 -0500

    added rotarymech for intake pivot

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -6,13 +6,16 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.W8.io.motor.MotorIO.PIDSlot;
 import frc.lib.W8.mechanisms.flywheel.FlywheelMechanism;
+import frc.lib.W8.mechanisms.rotary.RotaryMechanism;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  private FlywheelMechanism _io;
+  private FlywheelMechanism _rollerIO;
+  private RotaryMechanism _pivotIO;
 
-  public Intake(FlywheelMechanism io) {
-    _io = io;
+  public Intake(FlywheelMechanism rollerIO, RotaryMechanism pivotIO) {
+    _rollerIO = rollerIO;
+    _pivotIO = pivotIO;
   }
 
   public void setVelocity(double velocity) {
