6c6af581d28bfcb894db543edd6d748a6f02ad12
chris park <chrispy1729@gmail.com>
2026-03-08 13:41:12 -0400

    Added logger for Spindexer Supply Current

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.*;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.units.measure.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -39,5 +41,7 @@ public class Hopper extends SubsystemBase {
   }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    Logger.recordOutput("Spindexer/SupplyCurrent", _io.getSupplyCurrent());
+  }
 }
