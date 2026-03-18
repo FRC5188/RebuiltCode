f273d54650ef47d9114fb9c3f8e5d7286d6685cb
Katie Henthorn <113950897+kt-h@users.noreply.github.com>
2026-03-18 19:44:05 -0400

    Combine shoot on the move and vision

--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -36,7 +36,7 @@ public interface VisionIO {
   public static enum PoseObservationType {
     MEGATAG_1,
     MEGATAG_2,
-    PHOTONVISION
+    LIMELIGHT
   }
 
   public default void updateInputs(VisionIOInputs inputs) {}
